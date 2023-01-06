3939eae8cadc6d9e9c609490e6569e3248d783d7
rahmeesha reynolds <crazym.weird@gmail.com>
2023-01-06 09:55:08 -0500

    "js fixes"

---
+++ main.js
@@ -0,0 +1,51 @@
+function drawImage() {
+    const canvas = document.getElementById('horror')
+    const ctx = document.getContext('2d');
+    const img = new Image();
+    
+    ctx.drawImage(img, 0, 0, 50, 50);
+}   
+
+function copyImageToCanvas() {
+    const img = document.querySelector('img');
+    const canvas = document.querySelector('canvas');
+
+    const ctx = canvas.getContext('2d');
+
+    ctx.drawImage(img, 0, 0, 50, 20, 30, 30, 32, 32);
+
+}
+
+
+// ctx.drawImage(img, 
+// 0, 0, tilesheet x, y (top left corner of grab) 
+// 500, 20, how big of a grab
+// 0, 0, where you want the crop the be placed
+// 100, 100 size of placement of what was grabbed
+// ); 
+
+// setTimeout(() => {
+//    copyImageToCanvas(); 
+// }, 300);
+
+
+// class image {
+//     constructor(imagePath, xpos, ypos, width, height) {
+//         this.imagePath = imagePath;
+//         this.xpos = xpos;
+//         this.ypos = ypos;
+//         this.width = width;
+//         this.height = height;
+//     }
+// }
+
+// function createImage(context, imagePath, xpos, ypos, width, height) {
+//     const myImage = document.createElement('img');
+//     myImage.src = imagePath;
+//     myImage.onload = function() {
+//         context.drawImage(myImage, xpos, ypos, width, height);
+//     }
+// }
+
+// const image = new Image('patrick-perkins-DP4dLbZdybA-unsplash.jpg', 100, 50, 300, 200);
+// createImage(context, image.imagePath, image.xpos, image.ypos, image.width, image.height);
